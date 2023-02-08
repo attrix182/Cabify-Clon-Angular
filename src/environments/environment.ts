3cf9cdfdea9e68e567e03f76a78d025d37747614
Luciano Sinisterra <44885834+attrix182@users.noreply.github.com>
2023-02-08 01:47:46 -0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,10 +2,38 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
-  production: false,
-  apiKeyGoogle: process.env.API_KEY_GOOGLE
+
+const setEnv = () => {
+  const fs = require('fs');
+  const writeFile = fs.writeFile;
+// Configure Angular `environment.ts` file path
+  const targetPath = './src/environments/environment.ts';
+// Load node modules
+  const colors = require('colors');
+  const appVersion = require('../../package.json').version;
+  require('dotenv').config({
+    path: '.env'
+  });
+// `environment.ts` file structure
+  const envConfigFile = `export const environment = {
+  googleApiKey: '${process.env.API_KEY_GOOGLE}',
+  appVersion: '${appVersion}',
+  production: true,
 };
+`;
+  console.log(colors.magenta('The file `environment.ts` will be written with the following content: \n'));
+  writeFile(targetPath, envConfigFile, (err) => {
+    if (err) {
+      console.error(err);
+      throw err;
+    } else {
+      console.log(colors.magenta(`Angular environment.ts file generated correctly at ${targetPath} \n`));
+    }
+  });
+};
+
+setEnv();
+
 
 /*
  * For easier debugging in development mode, you can import the following file
