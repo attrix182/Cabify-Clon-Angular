6935069e4fe88dac3feca05becbcbf11acb856ad
Luciano S <luciano.sinisterra@lamercantil.com.ar>
2023-02-09 21:45:22 -0300

    Feat Mejoro modal route

--- src/app/components/ride-route-modal/ride-route-modal.component.ts
+++ src/app/components/ride-route-modal/ride-route-modal.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
+import { RideModalComponent } from '../ride-modal/ride-modal.component';
 
 @Component({
   selector: 'app-ride-route-modal',
@@ -12,6 +13,22 @@ export class RideRouteModalComponent implements OnInit {
   constructor(private modalCtrl:ModalController) { }
 
   ngOnInit() {}
+
+  closeModal(){
+    this.modalCtrl.dismiss();
+    this.modalCtrl.create({
+      component: RideModalComponent,
+      initialBreakpoint: 0.5,
+      breakpoints: [0.5, 0.25, 0.8],
+      backdropDismiss: false,
+      backdropBreakpoint: 0,
+      showBackdrop: false,
+    }
+    ).then(modal => {
+      modal.present();
+    }
+    );
+  }
 
   enterRoute(){
     this.modalCtrl.dismiss();
